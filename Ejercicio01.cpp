19675712183ce95057e9f46d618b927842351f77
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-07-07 07:12:08 -0500

    Ejercicio 01 - Version Final: Se soluciona algunos errores en los parametros de las funciones void y se mejora la interfaz del programa

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -5,41 +5,46 @@ resultado, y muestre las dos matrices y su resultado en horizontal */
 #include <iostream>
 using namespace std;
 
-void leermatrices(int matrizB[][3], int matrizA[][3]){
+void leermatrices(int matrizA[][3], int matrizB[][3]){
+    cout << "INGRESA LOs VALORES PARA LA MATRIZ A\n" << endl;
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
+            cout << "Ingresa el valor para la matriz[" << i << "][" << j << "]: "; 
             cin >> matrizA[i][j];
         }       
     }
+    cout << "\nINGRESE LOS VALORES PARA LA MATRIZ B\n" << endl;
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
+            cout << "Ingresa el valor para la matriz[" << i << "][" << j << "]:"; 
             cin >> matrizB[i][j];
         }
     }
 }
 
-void producto(int matrizB[][3], int matrizA[][3], int matrizC[][3]){
+void producto(int matrizA[][3], int matrizB[][3], int matrizC[][3]){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             matrizC[i][j] = 0;
             for (int k = 0; k < 3; k++){
                 matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
             }
-            
+
         }       
     }
 }
-void mostrarmatriz(int matrizB[][3], int matrizA[][3], int matrizC[][3]){
+void mostrarmatriz(int matrizA[][3], int matrizB[][3], int matrizC[][3]){
+    cout << "\nRESULTADO DEL PRODUCTO DE MATRICES\n";
+    cout << "             MATRIZ A               MATRIZ B            MATRIZ RESULTANTE\n";
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cout << matrizA[i][j] << " ";
-            cout << char(9);
+            cout << "\t" << matrizA[i][j];
         }
         for (int j = 0; j < 3; j++) {
-            cout << char(9) << matrizB[i][j] << " ";
+            cout << "\t" << matrizB[i][j];
         }
         for (int j = 0; j < 3; j++) {
-            cout << char(9) << matrizC[i][j] << " ";
+            cout << "\t" << matrizC[i][j];
         }
         cout << endl;
     }
