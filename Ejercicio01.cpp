8ad2060b4078313d40a9daee3af9c385fba67146
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-07-07 06:39:13 -0500

    Ejercicio 01 - Avance 2 : Se agrega una funcion para imprimir las dos matrices y su producto, y se soluciona errores en la funcion void leermatrices

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 void leermatrices(int matrizB[][3], int matrizA[][3]){
     for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; i++){
+        for (int j = 0; j < 3; j++){
             cin >> matrizA[i][j];
         }       
     }
     for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; i++){
+        for (int j = 0; j < 3; j++){
             cin >> matrizB[i][j];
         }
     }
@@ -25,16 +25,29 @@ void producto(int matrizB[][3], int matrizA[][3], int matrizC[][3]){
             for (int k = 0; k < 3; k++){
                 matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
             }
+            
         }       
     }
 }
-void mostrarmatriz(){
-    
+void mostrarmatriz(int matrizB[][3], int matrizA[][3], int matrizC[][3]){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrizA[i][j] << " ";
+            cout << char(9);
+        }
+        for (int j = 0; j < 3; j++) {
+            cout << char(9) << matrizB[i][j] << " ";
+        }
+        for (int j = 0; j < 3; j++) {
+            cout << char(9) << matrizC[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
-
 int main(){
     int matrizA[3][3], matrizB[3][3], matrizC[3][3];
     leermatrices(matrizA, matrizB);
     producto(matrizA, matrizB, matrizC);
+    mostrarmatriz(matrizA, matrizB, matrizC);
     return 0;
 }
